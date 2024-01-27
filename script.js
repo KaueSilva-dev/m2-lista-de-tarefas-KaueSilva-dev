019c46b2d10009b1218c13d4062f3b6133d938a8
kaue.silva <kaue.ps90@gmail.com>
2024-01-27 04:25:32 -0300

    :tada: feature: entrega do projeto

--- script.js
+++ script.js
@@ -1,73 +1,79 @@
-const tasks = [];
+const tasks = [
+  { title: "Comprar comida para o gato", type: "Urgente" },
+  { title: "Consertar Computador", type: "Importante" },
+  { title: "Beber água", type: "Normal" },
+  { title: "Enviar relatório trimestral", type: "Importante" },
+  { title: "Fazer exercícios físicos", type: "Normal" },
+  { title: "Agendar consulta médica", type: "Urgente" },
+  { title: "Ler pelo menos um capítulo de um livro", type: "Normal" },
+  { title: "Limpar a despensa", type: "Importante" },
+  { title: "Pagar a conta de energia", type: "Urgente" },
+  { title: "Assistir a um documentário interessante", type: "Normal" },
+];
 
 function renderElements(tasksArray) {
+  const ulTasks = document.querySelector(".tasks__list");
 
-  const ulTasks = document.querySelector('.tasks__list');
+  ulTasks.innerHTML = "";
 
-  ulTasks.innerHTML = '';
-
-  tasksArray.forEach(renderElements => {
+  tasksArray.forEach((renderElements) => {
     let cuurentTaskLi = createTasksItem(renderElements);
     ulTasks.append(cuurentTaskLi);
   });
-
 }
 
-
 function createTasksItem(tasksObject) {
-  const liTask = document.createElement('li');
-  const divTask = document.createElement('div');
-  const spanTask = document.createElement('span');
-  const pTask = document.createElement('p');
-  const buttonRemoveTask = document.createElement('button');
-  
-  liTask.classList.add('task__item');
-  divTask.classList.add('task-info__container');
-  spanTask.classList.add('task-type');
+  const liTask = document.createElement("li");
+  const divTask = document.createElement("div");
+  const spanTask = document.createElement("span");
+  const pTask = document.createElement("p");
+  const buttonRemoveTask = document.createElement("button");
+
+  liTask.classList.add("task__item");
+  divTask.classList.add("task-info__container");
+  spanTask.classList.add("task-type");
   pTask.innerHTML = tasksObject.title;
-  buttonRemoveTask.classList.add('task__button--remove-task');
-  
-  
-  if(tasksObject.type === 'normal') {
-    spanTask.classList.add('span-normal');
-  } else if(tasksObject.type === 'urgente') {
-    spanTask.classList.add('span-urgent');
-  } else if(tasksObject.type === 'importante') {
-    spanTask.classList.add('span-important');
+  buttonRemoveTask.classList.add("task__button--remove-task");
+
+  if (tasksObject.type === "normal") {
+    spanTask.classList.add("span-normal");
+  } else if (tasksObject.type === "urgente") {
+    spanTask.classList.add("span-urgent");
+  } else if (tasksObject.type === "importante") {
+    spanTask.classList.add("span-important");
   }
-  
-  buttonRemoveTask.addEventListener('click', () => {
+
+  buttonRemoveTask.addEventListener("click", () => {
     const index = tasks.indexOf(tasksObject);
-    
+
     tasks.splice(index, 1);
-    
-    renderElements(tasks)  
+
+    renderElements(tasks);
   });
 
   divTask.append(spanTask, pTask);
   liTask.append(divTask, buttonRemoveTask);
-  
+
   return liTask;
 }
 
+const buttonAddTask = document.querySelector(".form__button--add-task");
 
-  const buttonAddTask = document.querySelector('.form__button--add-task');
+buttonAddTask.addEventListener("click", (event) => {
+  event.preventDefault();
 
-  buttonAddTask.addEventListener('click', (event) => {
-    event.preventDefault();
-    
-    const inputText = document.querySelector('.form__input--text');
-    const inputPriority = document.querySelector('.form__input--priority');
-  
-    const newTask = {
-      title: inputText.value,
-      type: inputPriority.value,
-    }
+  const inputText = document.querySelector(".form__input--text");
+  const inputPriority = document.querySelector(".form__input--priority");
 
-    tasks.push(newTask);
+  const newTask = {
+    title: inputText.value,
+    type: inputPriority.value,
+  };
 
-    renderElements(tasks);
+  tasks.push(newTask);
 
-    inputText.value = ''
-    inputPriority.value = ''   
-  });
+  renderElements(tasks);
+
+  inputText.value = "";
+  inputPriority.value = "";
+});
